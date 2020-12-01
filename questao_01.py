aaaeff29130232ff54413d70b9c3cc2c6ad52d3a
franciscocamellon <franciscocamellon@gmail.com>
2020-12-01 08:38:41 -0300

    Finished

--- questao_01.py
+++ questao_01.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-/************************ TESTE DE PERFORMANCE 01 **************************
+/******************************* ASSESMENT *********************************
 *    Questao 01                                                            *
 *        Aluno           : Francisco Alves Camello Neto                    *
 *        Disciplina      : Fundamentos do Desenvolvimento Python           *
@@ -8,50 +8,34 @@
 *        Nome do arquivo : questao_01.py                                   *
 ***************************************************************************/
 """
-from validation import Validate
 
 
 class Questao_01():
     """
-    This program takes a number from user and return the sum of all prime
-    numbers from one to user input.
+    This program takes a tuple and prints your elements in ascending order.
     """
 
     def __init__(self):
-        """
-        Constructor
-        """
-        self.input = int
-        self.data = []
-        self.num = int
+        """ Constructor """
+        self.data = (55, 9, 22)
 
     def init_class(self):
-        """
-        This function receives the input data from users.
-        """
-
-        self.input = Validate().validate_values(' Digite um número: ')
+        """ This function receives the input data from users. """
+        print('Tupla original: {}'.format(self.data))
 
     def process_data(self):
         """ This function process the input data from init_class. """
-
         self.init_class()
-
-        for i in range(1, self.input):
-            if i % 2 == 0:
-                pass
-            else:
-                self.data.append(i)
-        self.num = sum(self.data)
+        _list = list(self.data)
+        _list.sort()
+        return tuple(_list)
 
     def print_result(self):
         """ This is a printer! It prints. """
 
         print('===' * 25, 'Questão 01'.center(75), '===' * 25, sep='\n')
-        self.process_data()
         print('---' * 25,
-              '  A soma dos números ímpares de 1 a {} é: {}'.format(
-                  self.input, self.num),
+              'Tupla em ordem crescente: {}'.format(self.process_data()),
               '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
