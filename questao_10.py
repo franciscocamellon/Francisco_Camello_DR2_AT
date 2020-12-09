38607db79ca06ce37ea44cfd9ce89c426b045d86
franciscocamellon <franciscocamellon@gmail.com>
2020-12-08 22:04:56 -0300

    Finished

--- questao_10.py
+++ questao_10.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-/************************ TESTE DE PERFORMANCE 01 **************************
+/******************************* ASSESSMENT *********************************
 *    Questao 10                                                            *
 *        Aluno           : Francisco Alves Camello Neto                    *
 *        Disciplina      : Fundamentos do Desenvolvimento Python           *
@@ -8,40 +8,47 @@
 *        Nome do arquivo : questao_10.py                                   *
 ***************************************************************************/
 """
-import turtle
-from validation import Validate
+import csv
+import requests
+# import pandas as pd
+
 
 class Questao_10():
     """ This function draws a square with side size given by the user. """
 
     def __init__(self):
         """ Constructor. """
-        turtle.title('Questão 10')
-        self.squirtle = turtle.Turtle()
-        self.squirtle.penup()
-        self.side_size, self.x, self.y = 0, 0, 0
-        self.input_txt = ' Digite o tamanho do lado do quadrado: '
-        self.data = {self.input_txt: 4}
+        self.url = 'https://sites.google.com/site/dr2fundamentospython/arquivos/Winter_Olympics_Medals.csv'
+        self.gold_medal = []
+        self.total_medal = {}
 
     def init_class(self):
         """ This function receives the input data from users. """
 
-        for k, v in self.data.items():
-            self.data[k] = Validate().validate_values(k, False)
+        self.csv = requests.get(self.url).text
+        self.csv_lines = self.csv.splitlines()
 
-        self.side_size = self.data.get(self.input_txt)
-        self.x = self.y = (self.data.get(self.input_txt) // 2) * (-1)
+        for line in self.csv_lines:
+            coluna = line.split(',')
+            if coluna[0] != 'Year' and int(coluna[0]) >= 2001:
+                if coluna[4] == 'SWE' or coluna[4] == 'DEN' or coluna[4] == 'NOR':
+                    if coluna[2] == 'Curling' or coluna[2] == 'Skating' or coluna[2] == 'Skiing' or coluna[2] == 'Ice Hockey':
+                        if coluna[7] == 'Gold':
+                            self.gold_medal.append(coluna)
 
     def process_data(self):
         """ This function process the input data from init_class. """
-
         self.init_class()
-        self.squirtle.setpos( self.x, self.y)
-        self.squirtle.pendown()
 
-        for sides in range(1, 5):
-            self.squirtle.forward(self.side_size)
-            self.squirtle.setheading(sides * 90)
+        country_gold_medal = {'SWE': 0, 'DEN': 0, 'NOR': 0}
+        for country in self.gold_medal:
+            if country[4] == 'SWE':
+                country_gold_medal['SWE'] += 1
+            if country[4] == 'DEN':
+                country_gold_medal['DEN'] += 1
+            if country[4] == 'NOR':
+                country_gold_medal['NOR'] += 1
+        self.total_medal = country_gold_medal
 
     def print_result(self):
         """
@@ -49,12 +56,10 @@ class Questao_10():
         """
         print('===' * 25, 'Questão 10'.center(75), '===' * 25, sep='\n')
         self.process_data()
-        print('---' * 25, 
-            ' Quadrado de lado {} desenhado com sucesso!'.format(self.side_size), 
-            '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
-
-        turtle.done()
-
+        print('---' * 25,
+              'O país com mais medalhas de ouro no século 21 nas modalidades Skiing,',
+              'Curling, Skating, Ice Hockey foi: {}.'.format(self.total_medal),
+              '---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
 Questao_10().print_result()
