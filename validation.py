18d8d391658b76422056a8b16d4107f9bed2cdd7
franciscocamellon <franciscocamellon@gmail.com>
2020-12-11 23:20:02 -0300

    Source

--- validation.py
+++ validation.py
@@ -25,16 +25,6 @@ class Equal_Length(Custom_Error):
     pass
 
 
-class Months(Custom_Error):
-    """ Raised when the input value is greater than 12. """
-    pass
-
-
-class Days(Custom_Error):
-    """ Raised when the input value is greater than 30. """
-    pass
-
-
 class String(Custom_Error):
     """ Raised when the error involves strings. """
     pass
@@ -46,22 +36,6 @@ class Not_Found(Custom_Error):
 
 
 class Validate(Custom_Error):
-
-    def validate_factorial(self, title):
-        """ Validates the input data given by the user. """
-
-        while True:
-            try:
-                value = int(input(title))
-                if value < 0:
-                    raise Less_Than_Zero
-                else:
-                    return value
-                break
-            except ValueError:
-                print('{}Erro! Digite um número inteiro positivo!'.format(' '*3))
-            except Less_Than_Zero:
-                print('{}Erro! Não é possível calcular fatorial de números negativos!'.format(' '*3))
 
     def validate_value_tuple(self, title, _tuple):
         """ Validates the input data given by the user. """
@@ -116,32 +90,8 @@ class Validate(Custom_Error):
             except ValueError:
                 print('{}Erro! Digite uma string!'.format(' '*3))
             except String:
-                print('{}Erro! String tem menos de {} caracteres!'.format(length, ' '*3))
+                print('{}Erro! String tem menos de {} caracteres!'.format(
+                    length, ' '*3))
             except Equal_Length:
                 print(
                     '{}Aviso! Tamanho da string igual ao número, nenhuma mudança será percebida!', sep="\n".format(' '*3))
-
-    def validate_age(self, title, years=False, months=False, days=False):
-        """ Validates the input data given by the user. """
-
-        while True:
-            try:
-                if months:
-                    _months = self.validate_values(title, True)
-                    if _months > 12:
-                        raise Months
-                    else:
-                        return _months
-                elif days:
-                    _days = self.validate_values(title, True)
-                    if _days > 30:
-                        raise Days
-                    else:
-                        return _days
-                break
-            except ValueError:
-                print('{}Erro! Digite um número inteiro!'.format(' '*3))
-            except Months:
-                print('{}Erro! Aquantidade de meses não pode ser maior que 12!'.format(' '*3))
-            except Days:
-                print('{}Erro! Aquantidade de dias não pode ser maior que 30!'.format(' '*3))
