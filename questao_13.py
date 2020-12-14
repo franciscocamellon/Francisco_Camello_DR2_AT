565ab64c4b263014b354fb81548d7e446a612a7f
franciscocamellon <franciscocamellon@gmail.com>
2020-12-14 14:36:57 -0300

    Finished

--- questao_13.py
+++ questao_13.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-/************************ TESTE DE PERFORMANCE 01 **************************
+/******************************* ASSESSMENT *********************************
 *    Questao 13                                                            *
 *        Aluno           : Francisco Alves Camello Neto                    *
 *        Disciplina      : Fundamentos do Desenvolvimento Python           *
@@ -8,59 +8,54 @@
 *        Nome do arquivo : questao_13.py                                   *
 ***************************************************************************/
 """
-import turtle
-from validation import Validate
+import requests
+import re
+from collections import Counter
+from bs4 import BeautifulSoup as bs
+
 
 class Questao_13():
     """ This function draws squares side by side. """
 
     def __init__(self):
         """ Constructor. """
-        turtle.title('Questão 13')
-        self.squirtle = turtle.Turtle()
-        self.squirtle.penup()
-        self.side_size, self.x, self.y = 0, 0, 0
-        self.distance_txt = ' Digite o tamanho do lado do primeiro quadrado: '
-        self.quantity_txt = ' Digite a quantidade de quadrados a serem desenhados: '
-        self.data = {self.quantity_txt: 0, self.distance_txt: 4}
+        self.words = 0
+        self.ladies = 0
+        self.words_list = []
+        self.only_once = []
 
     def init_class(self):
         """ This function receives the input data from users. """
-
-        for k, v in self.data.items():
-            self.data[k] = Validate().validate_values(k, False)
-
-        self.x = self.y = (self.data.get(self.distance_txt) // 2) * (-1)
+        self.request = requests.get('http://brasil.pyladies.com/about')
+        self.request.encoding = self.request.apparent_encoding
+        self.soup = bs(self.request.text, 'lxml')
+        for item in self.soup.html.body.article.find_all('div'):
+            self.words_list = [re.sub('\W+', '', word).lower()
+                               for word in item.text.split()]
+        self.words = len(self.words_list)
 
     def process_data(self):
         """ This function process the input data from init_class. """
-
         self.init_class()
-        self.squirtle.setpos(self.x, self.y)
-
-        _range = self.data.get(self.quantity_txt)
-        _distance = self.data.get(self.distance_txt)
-
-        for i in range(1, _range):
-            self.squirtle.pendown()
-            for sides in range(1, 5):
-                self.squirtle.forward(_distance * i*3)
-                self.squirtle.setheading(sides * 90)
-            self.squirtle.penup()
-            self.squirtle.goto(self.x * i*2.5, self.y * i*2.5)
-        
-        self.squirtle.goto(self.x, self.y)
+        word_dict = dict(Counter(self.words_list))
+        self.ladies = word_dict['ladies']
+        for key, value in word_dict.items():
+            if value == 1:
+                self.only_once.append(key)
 
     def print_result(self):
-        """
-        This is a printer! It prints.
-        """
+        """ This is a printer! It prints. """
+
         print('===' * 25, 'Questão 13'.center(75), '===' * 25, sep='\n')
         self.process_data()
-        print('---' * 25, ' Desenho finalizado com sucesso!', '---' *
-              25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
+        print('---' * 25,
+              '{}Existem {} palavras no conteúdo sobre as PyLadies, das quais\n {} aparecem uma única vez, enquanto que a palavra ladies é\n repetida {} vezes.\n'.format(
+                  ' ', self.words, len(self.only_once), self.ladies),
+              'Esta é a lista das palavras que ocorrem uma única vez no conteúdo:', sep="\n")
+        for word in self.words_list:
+            print(word)
 
-        turtle.done()
+        print('---' * 25, 'Aluno: Francisco Camello'.rjust(75), sep="\n")
 
 
 Questao_13().print_result()
